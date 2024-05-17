eba4181dcb8eefe251d8a8167a1ba64af1a4d9f7
tigawanna <denniskinuthiawaweru@gmail.com>
2024-05-17 18:48:55 +0300

    add basic chi router and logger

--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
 func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/", rootHandler)
+	router.Get("/hello", helloHandler)
 	server := &http.Server{
 		Addr:    ":8080",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	fmt.Println("Starting server...")
@@ -19,6 +26,9 @@ func main() {
 	}
 }
 
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Homeroute"))
+}
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello route"))
 }
