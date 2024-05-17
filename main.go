8d5547f4531c77efc5e35a8a6ba528a27acf723f
tigawanna <denniskinuthiawaweru@gmail.com>
2024-05-17 18:31:13 +0300

    basic web server

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	fmt.Println("Starting server...")
+	err := server.ListenAndServe()
+
+	if err != nil {
+		fmt.Println("Error starting server: ", err)
+	}
+}
+
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
 }
